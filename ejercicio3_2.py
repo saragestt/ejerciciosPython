338a35988e089dacb7322bb5bf1b0261fa6a71e6
saragestt <iamsaramusic@gmail.com>
2025-01-09 23:04:38 +0100

    ejercicio3_2.py

--- ejercicio3_2.py
+++ ejercicio3_2.py
@@ -23,43 +23,43 @@ elif pregunta_numero == 3:
     else:
         print("No es correcto")
 elif pregunta_numero == 4:
-    t4 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t4 = input("Dime la tabla del 4 (Solo el resultado):\n")
     if t4 == "4,8,12,16,20,24,28,32,36,40":
         print("Es correcto")
     else:
         print("No es correcto")
 elif pregunta_numero == 5:
-    t5 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t5 = input("Dime la tabla del 5 (Solo el resultado):\n")
     if t5 == "5,10,15,20,25,30,35,40,45,50":
         print("Es correcto")
     else:
         print("No es correcto")
 elif pregunta_numero == 6:
-    t6 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t6 = input("Dime la tabla del 6 (Solo el resultado):\n")
     if t6 == "6,12,18,24,30,36,42,48,54,60":
         print("Es correcto")
     else:
         print("No es correcto")
 elif pregunta_numero == 7:
-    t7 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t7 = input("Dime la tabla del 7 (Solo el resultado):\n")
     if t7 == "7,14,21,28,35,42,49,56,63,70":
         print("Es correcto")
     else:
         print("No es correcto")
 elif pregunta_numero == 8:
-    t8 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t8 = input("Dime la tabla del 8 (Solo el resultado):\n")
     if t8 == "8,16,24,32,40,48,56,64,72,80":
         print("Es correcto")
     else:
         print("No es correcto")
 elif pregunta_numero == 9:
-    t9 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t9 = input("Dime la tabla del 9 (Solo el resultado):\n")
     if t9 == "9,18,27,36,45,54,63,72,81,90":
         print("Es correcto")
     else:
         print("No es correcto")
 elif pregunta_numero == 10:
-    t10 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    t10 = input("Dime la tabla del 10 (Solo el resultado):\n")
     if t10 == "10,20,30,40,50,60,70,80,90,100":
         print("Es correcto")
     else:
