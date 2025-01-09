ac21823e8d53e636616033359369886477cc25b5
saragestt <iamsaramusic@gmail.com>
2025-01-09 23:02:14 +0100

    Add files via upload

---
+++ ejercicio3_2.py
@@ -0,0 +1,68 @@
+#Captura por teclado un número y queremos que nos pregunte la tabla de multiplicar de
+#dicho número. Nosotros responderemos y el programa indicara si es correcto o no, lo que
+#hemos escrito
+
+pregunta_numero = int(input("Introduce un numero:\n"))
+
+if pregunta_numero == 1:
+    t1 = input("Dime la tabla del 1 (Solo el resultado):\n")
+    if t1 == "1,2,3,4,5,6,7,8,9,10":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 2:
+    t2 = input("Dime la tabla del 2 (Solo el resultado):\n")
+    if t2 == "2,4,6,8,10,12,14,16,18,20":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 3:
+    t2 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t3 == "3,6,9,12,15,18,21,24,27,30":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 4:
+    t4 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t4 == "4,8,12,16,20,24,28,32,36,40":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 5:
+    t5 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t5 == "5,10,15,20,25,30,35,40,45,50":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 6:
+    t6 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t6 == "6,12,18,24,30,36,42,48,54,60":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 7:
+    t7 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t7 == "7,14,21,28,35,42,49,56,63,70":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 8:
+    t8 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t8 == "8,16,24,32,40,48,56,64,72,80":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 9:
+    t9 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t9 == "9,18,27,36,45,54,63,72,81,90":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+elif pregunta_numero == 10:
+    t10 = input("Dime la tabla del 3 (Solo el resultado):\n")
+    if t10 == "10,20,30,40,50,60,70,80,90,100":
+        print("Es correcto")
+    else:
+        print("No es correcto")
+
+
